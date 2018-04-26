e4d3c1da6d29089e8677ce67d7cb4646926d929e
Salinger MS <minsiua@gmail.com>
2018-04-27 04:58:14 +0900

    add menu template

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, Menu, ipcMain, dialog } from 'electron';
 import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
 import { enableLiveReload } from 'electron-compile';
+import path from 'path';
+import fs from 'fs';
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -16,6 +18,7 @@ const createWindow = async () => {
   mainWindow = new BrowserWindow({
     width: 1600,
     height: 900,
+    title: 'Moum v0.1.0'
   });
 
   // and load the index.html of the app.
@@ -26,12 +29,14 @@ const createWindow = async () => {
     await installExtension(REACT_DEVELOPER_TOOLS);
     mainWindow.webContents.openDevTools();
   }
-
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
   // Emitted when the window is closed.
   mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
+    app.quit();
     mainWindow = null;
   });
 };
@@ -60,3 +65,213 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+//ipcMain on functions
+let newFontOptionsWindow = null;
+
+function newFontWindow(){
+  newFontOptionsWindow = new BrowserWindow({
+    width: 400,
+    height: 250,
+    menu: null,
+    
+  });
+  newFontOptionsWindow.loadURL(`file://${__dirname}/windows/newFontOptionsWindow.html`);
+  newFontOptionsWindow.once('ready-to-show', ()=>{ newFontOptionsWindow.show(); })
+}
+ipcMain.on('new-font', (event, fontName)=>{
+  // console.log('new-font activated in index.js')
+  // console.log(event.sender);
+  mainWindow.webContents.send('addTab', fontName)
+})
+const newFontTab = (filename)=>{
+  mainWindow.webContents.send('open-font', filename)
+}
+function openFont(){
+  dialog.showOpenDialog({
+    title: 'Open Font',
+    defaultPath: path.join(require("os").homedir(),'/Documents'),
+    properties: [
+      'showHiddenFiles', 
+    ],
+    filters: [
+      { name: 'All Fonts', extensions: ['woff', 'ttf', 'otf']},
+      { name: 'Web Fonts', extensions: ['woff'] },
+      { name: 'True Type Fonts', extensions: ['ttf'] },
+      { name: 'Open Type Fonts', extensions: ['otf'] }
+    ],
+  },newFontTab
+)}
+// mainWindow.webContents.on('new-font', (event, fontName)=>{
+//   ipcMain.send('new-font', fontName);
+// })
+
+//Menu template
+const menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'New Font',
+        accelerator: process.platform === 'darwin' ? 'Command+N' : 'Ctrl+N',
+        click(){
+          console.log('New Font Menu clicked');
+          newFontWindow();
+        },
+        id: '_newFont'
+      },
+      {
+        label: 'Open Project',
+        accelerator: process.platform === 'darwin' ? 'Command+O' : 'Ctrl+O',
+        click(){
+          console.log('Open Project Menu clicked')
+          openFont();
+        },
+        id: '_openProject'
+      },
+      {
+        label: 'Save',
+        accelerator: process.platform === 'darwin' ? 'Command+S' : 'Ctrl+S',
+        click(){console.log('Save Menu clicked')},
+        id: '_save'
+      },
+      {
+        label: 'Save As...',
+        accelerator: process.platform === 'darwin' ? 'Command+Shift+S' : 'Ctrl+Shift+S',
+        click(){console.log('Save As Menu clicked')},
+        id: '_saveAs'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Import',
+        accelerator: process.platform === 'darwin' ? 'Command+Shift+O' : 'Ctrl+Shift+O',
+        click(){console.log('Import Font Menu clicked')},
+        id: '_import'
+      },
+      {
+        label: 'Export',
+        click(){console.log('Export Font Menu clicked')},
+        id: '_export'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Options',
+        accelerator: 'F10',
+        click(){console.log('Options Menu clicked')},
+        id: '_options'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Quit',
+        accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click(){app.quit();},
+        id: '_quit'
+      }
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        label: 'Undo',
+        accelerator: process.platform === 'darwin' ? 'Command+Z' : 'Ctrl+Z',
+        click(){console.log('Undo Menu clicked')},
+        id: '_undo',
+        enabled: false
+      },
+      {
+        label: 'Redo',
+        accelerator: process.platform === 'darwin' ? 'Command+Y' : 'Ctrl+Y',
+        click(){console.log('Redo Menu clicked')},
+        id: '_redo',
+        enabled: false
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Copy',
+        accelerator: process.platform === 'darwin' ? 'Command+C' : 'Ctrl+C',
+        click(){console.log('Copy Menu clicked')},
+        id: '_copy',
+        enabled: false
+      },
+      {
+        label: 'Paste',
+        accelerator: process.platform === 'darwin' ? 'Command+V' : 'Ctrl+V',
+        click(){console.log('Paste Menu clicked')},
+        id: '_paste',
+        enabled: false
+      },
+      {
+        label: 'Paste In Place',
+        accelerator: process.platform === 'darwin' ? 'Command+Shift+V' : 'Ctrl+Shift+V',
+        click(){console.log('Paste In Place Menu clicked')},
+        id: '_pasteInPlace',
+        enabled: false
+      },
+      {
+        type: 'separator'
+      }
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Show Layers',
+        id: '_showLayers',
+        submenu: [
+          {
+            label: 'Grid',
+            accelerator: process.platform === 'darwin' ? "Command+'" : "Ctrl+'",
+            click(){console.log('View Grid clicked')},
+            id: '_grid',
+            enabled: false
+          },
+          {
+            label: 'Ruler',
+            accelerator: process.platform === 'darwin' ? 'Command+R' : 'Ctrl+R',
+            click(){console.log('View Ruler clicked')},
+            id: '_ruler',
+            enabled: false
+          },
+          {
+            label: 'Glyph Metrics',
+            accelerator: process.platform === 'darwin' ? 'Command+"' : 'Ctrl+"',
+            click(){console.log('View Glyph Metrics clicked')},
+            id: '_glyphMetrics',
+            enabled: false
+          },
+        ]
+      }
+    ]
+  }
+]
+
+if (process.env.NODE_ENV !== 'production'){
+  menuTemplate.push({
+    label: 'Developer',
+    submenu: [
+      {
+        label: 'Devtools',
+        accelerator: 'F12',
+        click(){
+          mainWindow.webContents.openDevTools();
+        }
+      },
+      {role: 'toggledevtools'},
+      {role: 'reload'},
+      {role: 'forcereload'},
+      {role: 'togglefullscreen'},
+      {role: 'minimize'},
+      {role: 'close'}
+    ]
+  })
+}
--- src/tabs/tabs.js
+++ src/tabs/tabs.js
@@ -2,71 +2,87 @@ import TabGroup from 'electron-tabs';
 import electron from 'electron';
 import path from 'path';
 
-    const welcomeTabOptions = {
-        title: 'Welcome',
-        src: './tabs/welcome.html',
-        webviewAttributes: {
-            'nodeintegration': true
-        },
-        icon: 'fa',
-        iconURL: path.join(__dirname,'../assets/icons/arrow_drop.svg'),
-        visible: true,
-        closable: true,
-        active: true,
-        ready: tab => {
-            // Open dev tools for webview
-            let webview = tab.webview;
-            if (!!webview) {
-                webview.addEventListener('dom-ready', () => {
-                    webview.openDevTools();
-                })
-            }
+const { ipcRenderer } = electron;
+
+const welcomeTabOptions = {
+    title: 'Welcome',
+    src: './tabs/welcome.html',
+    webviewAttributes: {
+        'nodeintegration': true
+    },
+    icon: 'fa',
+    iconURL: path.join(__dirname,'../assets/icons/arrow_drop.svg'),
+    visible: true,
+    closable: true,
+    active: true,
+    ready: tab => {
+        // Open dev tools for webview
+        let webview = tab.webview;
+        if (!!webview) {
+            webview.addEventListener('dom-ready', () => {
+                webview.openDevTools();
+            })
         }
     }
-    const defaultTabOptions = {
-        title: 'Font.ttf',
-        src: './tabs/canvas.html',
-        webviewAttributes: {
-            'nodeintegration': true
-        },
-        iconURL: '../assets/icons/arrow-drop.svg',
-        visible: true,
-        closable: true,
-        active: false,
-        ready: tab => {
-            // Open dev tools for webview
-            let webview = tab.webview;
-            if (!!webview) {
-                webview.addEventListener('dom-ready', () => {
-                    // webview.openDevTools();
-                })
-            }
+}
+const defaultTabOptions = {
+    title: 'Font.ttf',
+    src: './tabs/canvas.html',
+    webviewAttributes: {
+        'nodeintegration': true
+    },
+    iconURL: '../assets/icons/arrow_drop.svg',
+    visible: true,
+    closable: true,
+    active: true,
+    ready: tab => {
+        // Open dev tools for webview
+        let webview = tab.webview;
+        if (!!webview) {
+            webview.addEventListener('dom-ready', () => {
+                // webview.openDevTools();
+            })
         }
     }
-    
-    const inspectionTabOptions = {
-        title: 'Home',
-        src: './tabs/inspector.html',
-        webviewAttributes: {
-            'nodeintegration': true
-        },
-        icon: 'fa fa-home',
-        visible: true,
-        closable: true,
-        active: true,
-        ready: tab => {
-            // Open dev tools for webview
-            let webview = tab.webview;
-            if (!!webview) {
-                webview.addEventListener('dom-ready', () => {
-                    // webview.openDevTools();
-                })
-            }
+}
+
+const inspectionTabOptions = {
+    title: 'Home',
+    src: './tabs/inspector.html',
+    webviewAttributes: {
+        'nodeintegration': true
+    },
+    icon: 'fa fa-home',
+    visible: true,
+    closable: true,
+    active: true,
+    ready: tab => {
+        // Open dev tools for webview
+        let webview = tab.webview;
+        if (!!webview) {
+            webview.addEventListener('dom-ready', () => {
+                // webview.openDevTools();
+            })
         }
-    }    
+    }
+}    
 
 let tabGroup = new TabGroup();
 tabGroup.addTab(welcomeTabOptions);
 // tabGroup.addTab(defaultTabOptions);
 
-export default tabGroup;
+// export default tabGroup;
+
+ipcRenderer.on('addTab', (event, fontName)=>{
+    console.log('addTab activated in tabs.js')
+    var options = defaultTabOptions;
+    options.title = fontName+'.ttf'
+    tabGroup.addTab(options);
+})
+
+ipcRenderer.on('open-font', (event, filename)=>{
+    console.log('open-font activated in tabs.js')
+    var options = defaultTabOptions;
+    options.title = path.basename(filename);
+    tabGroup.addTab(options);
+})
