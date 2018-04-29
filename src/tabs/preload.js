a2b6a610b4e15752c518d4d438142b5227e0ffb8
Salinger MS <minsiua@gmail.com>
2018-04-29 21:40:31 +0900

    canvas ipc communication success

--- src/tabs/preload.js
+++ src/tabs/preload.js
@@ -1,9 +1,13 @@
 const electron = require('electron');
+const paper = require('paper');
+const opentype = require('opentype.js');
 const {ipcRenderer} = electron;
 
 console.log('preload.js activated')
 
+let myFont = null;
+
 ipcRenderer.on('load-on-canvas', (event, props)=>{
     console.log('receiving load-on-canvas in canvas.js'+Date.now())
-    console.log(props);
+    myFont = opentype.loadSync(props[0]);
 })
--- src/tabs/tabs.js
+++ src/tabs/tabs.js
@@ -9,7 +9,6 @@ const welcomeTabOptions = {
     src: './tabs/welcome.html',
     webviewAttributes: {
         'nodeintegration': true,
-        'preload': './tabs/preload.js'
     },
     icon: 'fa',
     iconURL: path.join(__dirname,'../assets/icons/arrow_drop.svg'),
@@ -30,7 +29,8 @@ const defaultTabOptions = {
     title: 'Font.ttf',
     src: './tabs/canvas.html',
     webviewAttributes: {
-        'nodeintegration': true
+        'nodeintegration': true,
+        'preload': './tabs/preload.js'
     },
     iconURL: '../assets/icons/arrow_drop.svg',
     visible: true,
@@ -83,19 +83,36 @@ ipcRenderer.on('addTab', (event, fontName)=>{
 let newTab = null;
 let tempFileName = null;
 let tempTabId = null;
+
+let webviews = null;
+let webview = null;
+
 ipcRenderer.on('open-font', (event, filename)=>{
     console.log('open-font activated in tabs.js'+Date.now())
     var options = defaultTabOptions;
     // options.
     tempFileName = path.basename(filename[0]);
     options.title = tempFileName;
+    // newTab.
     newTab = tabGroup.addTab(options)
     setTimeout(()=>{
         tempTabId = newTab.id;
         console.log('sending load-font-on-canvas...'+Date.now())
-        ipcRenderer.send('load-font-on-canvas', [tempFileName, tempTabId])
-    },10000)
+            webviews = document.querySelectorAll('webview')
+            for(var i = 0; i< webviews.length; i++){
+                webview = webviews[i]
+                if(webview!= null){
+                    console.log(webview)
+                    webview.addEventListener('ipc-message', (event)=>{
+                        console.log(event.channel);
+                        console.log(`got load-on-canvas via tabs.js and eventListener in webview (id:{webview.id}) `+webview.id);
+                    })
+                    webview.send('load-on-canvas', [tempFileName, filename])
+                }
+            }
+        },1000)
 })
+
 // newTab.on("webview-ready", (tab) => {
 //     console.log('sending load-font-on-canvas...')
 //     ipcRenderer.send('load-font-on-canvas', [tempFileName, newTabId])
