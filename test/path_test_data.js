85ff52792b406d9a7bf8fdd88a6a12c95f4c263b
Salinger MS <minsiua@gmail.com>
2018-05-31 17:17:28 +0900

    add path5(ㄱ) for test

--- test/path_test_data.js
+++ test/path_test_data.js
@@ -19,7 +19,8 @@ var path2 = new p.Path({
         [0,200]
     ],
     closed: true,
-    selected: true
+    selected: true,
+    position: new p.Point(350,100)
 })
 
 // Predicted test results
@@ -48,14 +49,48 @@ var path3 = new p.Path({
         })
     ],
     closed: true,
-    selected: true
+    selected: true,
+    position: new p.Point(600,100)
 })
 
 var path4 = new p.Path({
     closed: true,
-    selected: true
+    selected: true,
 })
-path4.moveTo(0,400)
-path4.lineTo(200,450)
-path4.lineTo(100,500)
-path4.lineTo(0,450)
+path4.moveTo(0,0)
+path4.lineTo(200,50)
+path4.lineTo(100,100)
+path4.lineTo(0,50)
+path4.setPosition(new p.Point(100,300))
+
+var path5 = new p.Path({
+    closed: true,
+    selected: true,
+})
+path5.moveTo(0,0)
+path5.lineTo(20,10)
+path5.lineTo(100,0)
+path5.lineTo(105,-5)
+path5.lineTo(130,10)
+path5.cubicCurveTo([130,60],[50,210],[0,210])
+path5.lineTo(0,200)
+path5.cubicCurveTo([40,200],[100,45],[100,15])
+path5.lineTo(40,20)
+path5.lineTo(30,30)
+path5.lineTo(0,10)
+path5.lineTo(0,0)
+path5.position = [500,500]
+path5.scale(2)
+
+function addNumberToSegments(path){
+    var tempGroup = new p.Group()
+    path.segments.forEach((obj,idx)=>{
+        tempGroup.addChild(
+            new p.PointText({
+                point: new p.Point(obj.point.x+20,obj.point.y-20),
+                content: idx,
+                fillColor: 'black'
+            })
+        )
+    })
+}
