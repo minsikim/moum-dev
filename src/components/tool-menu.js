71964907c03954ae1c379687de867efff58e6d56
Salinger MS <minsiua@gmail.com>
2018-06-01 03:03:17 +0900

    shrink tool menu for now

--- src/components/tool-menu.js
+++ src/components/tool-menu.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const toolMenu = (props) => {
     const style= {
-        width: '250px',
+        width: '50px',
         backgroundColor: '#292929',
         height: '100%'
     }
